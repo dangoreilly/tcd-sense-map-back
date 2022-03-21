fdb10486e6d72e9a2acf297527a7652f40a2af66
Dan O'Reilly <dangoreilly@gmail.com>
2022-03-21 16:35:35 +0000

    Re-init

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '59fb6613c992daffcee6c1df9408fb59'),
+    secret: env('ADMIN_JWT_SECRET', '2fb3ae08011574fa6576f26024dbefe6'),
   },
 });
