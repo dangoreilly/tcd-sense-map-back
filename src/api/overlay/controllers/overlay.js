5d52251e2fbe6958c8ccf38162980e1ea6e0a5fd
Dan O'Reilly <dangoreilly@gmail.com>
2022-06-23 01:19:01 +0100

    Added overlay content type

---
+++ src/api/overlay/controllers/overlay.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  overlay controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::overlay.overlay');
